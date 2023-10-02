1d7a28b5203ca034bcd4f4e3092c3df8e2a23f2d
Valentim_Gab <gabrielvalentimxavier@gmail.com>
2023-10-01 22:42:34 -0300

    Correções projeto

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,15 +8,15 @@ export default function About() {
     <main className="about min-h-screen flex flex-col items-center px-5">
       <h1>Sobre o projeto</h1>
       <p>
-        Site que apresenta os detalhes dos personagens chamados de "Pokémon" da
-        franquia Pokémon.
+        Site que apresenta os detalhes dos personagens chamados de
+        &quot;Pokémon&quot; da franquia Pokémon.
       </p>
       <p>
         Fazendo o curso Hora de Codar Next.js, mas adaptando para Next.js 13.
       </p>
       <p>
         <strong className="flex items-center justify-center gap-1">
-          Criado por:{' '}
+          Criado por:
           <Link href="https://github.com/Valentim-Gab" target="_blank">
             Gabriel Valentim
           </Link>
